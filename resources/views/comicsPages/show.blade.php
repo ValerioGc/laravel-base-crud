593c5fe2f96744d4689958eb027bcc01baeaed05
ValerioGc <valerio.gc1@gmail.com>
2022-10-04 17:55:18 +0200

    Fine stilizzazione pagina dettaglio

--- resources/views/comicsPages/show.blade.php
+++ resources/views/comicsPages/show.blade.php
@@ -5,29 +5,31 @@
 @section('content')
     <div class="container">
             <ul class="comic">
-                    <li>
-                        <img src="{{$comic['thumb']}}" alt="Copertina {{$comic['title']}}">
-                    </li>
-                    <li>
-                        <h3>{{$comic['title']}}</h3>
-                    </li>
-                    <li>
-                        <p class="info-comic"><em>Serie:</em></p>
-                        <p>{{$comic['series']}}</p>
-                    </li>
-                    <li>
-                        <p class="info-comic">Prezzo:</p>
-                        <p>{{$comic['price']}}</p>
-                    </li>
-                    <li>
-                        <p class="info-comic">Data uscita:</p>
-                        <p>{{$comic['sale_date']}}</p>
-                    </li>
-                    <li>
-                        <p class="info-comic">Descrizione:</p>
-                        <p>{{$comic['description']}}</p>
-                    </li>
+                <li>
+                    <img src="{{$comic->thumb}}" alt="Copertina {{$comic['title']}}">
+                </li>
+                <li>
+                    <h3>{{$comic->title}}</h3>
+                </li>
+                <li>
+                    <p class="info-comic"><em>Serie:</em></p>
+                    <p>{{$comic->series}}</p>
+                </li>
+                <li>
+                    <p class="info-comic">Prezzo:</p>
+                    <p>{{$comic->price}}</p>
+                </li>
+                <li>
+                    <p class="info-comic">Data uscita:</p>
+                    <p>{{$comic->sale_date}}</p>
+                </li>
+                <li>
+                    <p class="info-comic">Descrizione:</p>
+                    <p>{{$comic->description}}</p>
+                </li>
+                <li>
+                    <a class="btn" href="{{route('comics.index')}}">Torna ai fumetti</a>
+                </li>
             </ul>
-            <a class="btn" href="{{route('comics.index')}}">Torna ai fumetti</a>
     </div>
 @endsection
