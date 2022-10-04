30c21fb419334995b73d52248ac8172832267cab
ValerioGc <valerio.gc1@gmail.com>
2022-10-04 17:10:42 +0200

    Stilizzazione Pagina dettaglio

--- resources/views/comicsPages/show.blade.php
+++ resources/views/comicsPages/show.blade.php
@@ -4,8 +4,7 @@
 
 @section('content')
     <div class="container">
-            <div class="comics">
-                <ul>
+            <ul class="comic">
                     <li>
                         <img src="{{$comic['thumb']}}" alt="Copertina {{$comic['title']}}">
                     </li>
@@ -13,23 +12,22 @@
                         <h3>{{$comic['title']}}</h3>
                     </li>
                     <li>
-                        <p><em>Serie:</em></p>
+                        <p class="info-comic"><em>Serie:</em></p>
                         <p>{{$comic['series']}}</p>
                     </li>
                     <li>
-                        <p>Prezzo:</p>
+                        <p class="info-comic">Prezzo:</p>
                         <p>{{$comic['price']}}</p>
                     </li>
                     <li>
-                        <p>Data uscita:</p>
+                        <p class="info-comic">Data uscita:</p>
                         <p>{{$comic['sale_date']}}</p>
                     </li>
                     <li>
-                        <p>Descrizione:</p>
+                        <p class="info-comic">Descrizione:</p>
                         <p>{{$comic['description']}}</p>
                     </li>
-                </ul>
-                <a class="btn" href="{{route('comics.index')}}">Torna ai fumetti</a>
-            </div>
+            </ul>
+            <a class="btn" href="{{route('comics.index')}}">Torna ai fumetti</a>
     </div>
 @endsection
