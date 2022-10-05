9ffe9442651ede67c495ebcb73ee7caca9e84e61
ValerioGc <valerio.gc1@gmail.com>
2022-10-05 22:51:31 +0200

    Aggiunti messaggi errore dettaglio

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Comic;
 
-use Illuminate\Validation\Rule;
-
 use Illuminate\Http\Request;
 
 
@@ -45,10 +43,31 @@ class ComicsController extends Controller
                 'title' => 'required|max:50|min:2',
                 'description' => 'nullable',
                 'thumb' => ['nullable', 'url'],
-                'price' => 'numeric|max:4|min:2',
-                'series' => 'required|max:10|min:5',
-                'sale_date' => 'date',
+                'price' => 'required|numeric|max:6',
+                'series' => 'required|max:20|min:5',
+                'sale_date' => 'required|date',
                 'type' => 'required|max:30|min:5',
+            ],
+            [
+              'title.required' => 'Inserisci il titolo',
+              'title.max' => 'Il titolo deve contenere massimo 50 caratteri',
+              'title.min' => 'Il titolo deve contenere minimo 2 caratteri',
+
+              'thumb.url' => 'Inserisci il link correttamente',
+
+              'price.required' => 'Inserisci il prezzo',
+              'price.max' => 'Il prezzo deve contenere massimo 6 cifre',
+
+              'series.required' => 'Inserisci la serie',
+              'series.max' => 'La serie deve contenere massimo 20 caratteri',
+              'series.min' => 'La serie deve contenere minimo 5 caratteri',
+
+              'sale_date.required' => 'Inserisci la data',
+              'sale_date.date' => 'Inserisci la data correttamente',
+
+              'type.required' => 'Inserisci il tipo',
+              'type.max' => 'Il tipo deve contenere massimo 30 caratteri',
+              'type.min' => 'Il tipo deve contenere minimo 5 caratteri',
             ]
         );
         $data = $request->all();
@@ -91,31 +110,49 @@ class ComicsController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
-        if ($comic) {
 
 
-            $request->validate(
+        $request->validate(
+            [
                 [
                     'title' => 'required|max:50|min:2',
                     'description' => 'nullable',
                     'thumb' => ['nullable', 'url'],
-                    'price' => 'numeric|max:4|min:2',
-                    'series' => 'required|max:10|min:5',
-                    'sale_date' => 'date',
+                    'price' => 'required|numeric|max:6',
+                    'series' => 'required|max:20|min:5',
+                    'sale_date' => 'required|date',
                     'type' => 'required|max:30|min:5',
+                ],
+                [
+                    'title.required' => 'Inserisci il titolo',
+                    'title.max' => 'Il titolo deve contenere massimo 50 caratteri',
+                    'title.min' => 'Il titolo deve contenere minimo 2 caratteri',
+
+                    'thumb.url' => 'Inserisci il link correttamente',
+
+                    'price.required' => 'Inserisci il prezzo',
+                    'price.max' => 'Il prezzo deve contenere massimo 6 cifre',
+
+                    'series.required' => 'Inserisci la serie',
+                    'series.max' => 'La serie deve contenere massimo 20 caratteri',
+                    'series.min' => 'La serie deve contenere minimo 5 caratteri',
+
+                    'sale_date.required' => 'Inserisci la data',
+                    'sale_date.date' => 'Inserisci la data correttamente',
+
+                    'type.required' => 'Inserisci il tipo',
+                    'type.max' => 'Il tipo deve contenere massimo 30 caratteri',
+                    'type.min' => 'Il tipo deve contenere minimo 5 caratteri',
                 ]
-            );
+            ]
+        );
 
-            $data = $request->all();
+        $data = $request->all();
 
-            $comic->update($data);
-            $comic->save();
+        $comic->update($data);
+        $comic->save();
 
-            return redirect()->route('comics.edit', ['comic' => $comic -> id])->with('status', 'Dato inserito correttamente');
-
-        } else {
-            abort(404);
-        }
+        return redirect()->route('comics.edit', ['comic' => $comic -> id])->with('status', 'Dato inserito correttamente');
 
     }
 
