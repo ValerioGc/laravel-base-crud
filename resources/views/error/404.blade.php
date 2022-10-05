57b49dd936dc06cea502902cef071d8079c5b3f7
ValerioGc <valerio.gc1@gmail.com>
2022-10-05 21:33:56 +0200

    Aggiunta pagina errore

---
+++ resources/views/error/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('page-title', '404 - Risorsa non trovata')
+
+@section('content')
+    <div>
+        <h1>Risorsa non trovata</h1>
+        <h3>Errore 404</h3>
+    </div>
+@endsection
